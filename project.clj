de3bf52f3a0719b9bbceb02732f14ecbd2ae8ec5
Carl Düvel <carl.duevel@otto.de>
2017-01-26 08:46:40 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
         :url  "https://github.com/otto-de/tesla-jetty"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-jetty-adapter "1.5.0"]]
+                 [ring/ring-jetty-adapter "1.5.1"]]
   :lein-release {:deploy-via :clojars}
   :exclusions [org.clojure/clojure
                org.slf4j/slf4j-nop
@@ -15,12 +15,12 @@
                log4j
                commons-logging/commons-logging]
 
-  :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.3.36"]
-                                       [com.stuartsierra/component "0.3.1"]]}
+  :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.7.1"]
+                                       [com.stuartsierra/component "0.3.2"]]}
              :dev      {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                       [org.slf4j/slf4j-api "1.7.21"]
-                                       [ch.qos.logback/logback-core "1.1.7"]
-                                       [ch.qos.logback/logback-classic "1.1.7"]
+                                       [org.slf4j/slf4j-api "1.7.22"]
+                                       [ch.qos.logback/logback-core "1.1.9"]
+                                       [ch.qos.logback/logback-classic "1.1.9"]
                                        [ring-mock "0.1.5"]]
                         :plugins      [[lein-ancient "0.6.10"][lein-release/lein-release "1.0.9"]]}}
   :test-paths ["test" "test-resources"])
