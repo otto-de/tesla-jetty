dc1db925d9f77d36f3fdedd752924f4c3d020169
Carl Düvel <carl.duevel@otto.de>
2017-10-23 16:29:05 +0200

    lein-release plugin: bumped version from 0.2.2 to 0.2.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-jetty "0.2.2"
+(defproject de.otto/tesla-jetty "0.2.3-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-jetty"
   :license {:name "Apache License 2.0"
