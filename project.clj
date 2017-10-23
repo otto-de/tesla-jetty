1bca65df3b070a57713f6567b0cafeb3686ca432
Carl Düvel <carl.duevel@otto.de>
2017-10-23 16:24:00 +0200

    Updated deps

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-jetty-adapter "1.6.2"]
-                 [io.prometheus/simpleclient_jetty "0.0.26"]]
+                 [io.prometheus/simpleclient_jetty "0.1.0"]]
   :lein-release {:deploy-via :clojars}
   :exclusions [org.clojure/clojure
                org.slf4j/slf4j-nop
@@ -17,7 +17,7 @@
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
 
-  :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.11.10"]
+  :profiles {:provided {:dependencies [[de.otto/tesla-microservice "0.11.12"]
                                        [com.stuartsierra/component "0.3.2"]]}
              :dev      {:dependencies [[org.slf4j/slf4j-api "1.7.25"]
                                        [ch.qos.logback/logback-core "1.2.3"]
