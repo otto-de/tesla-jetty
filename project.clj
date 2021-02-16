153588d6a730f66425a993d0664ac4702a083750
Carl Düvel <c.a.duevel@gmail.com>
2021-02-16 14:47:42 +0100

    lein-release plugin: preparing 0.2.8 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-jetty "0.2.8-SNAPSHOT"
+(defproject de.otto/tesla-jetty "0.2.8"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-jetty"
   :license {:name "Apache License 2.0"
