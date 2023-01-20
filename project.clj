c91dfd8822721fb6ab6f92173978602c2d80aff0
Paul Kobow <paul-vincent.kobow@otto.de>
2023-01-20 15:57:43 +0100

    lein-release plugin: bumped version from 0.2.11 to 0.2.12-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.otto/tesla-jetty "0.2.11"
+(defproject de.otto/tesla-jetty "0.2.12-SNAPSHOT"
   :description "basic microservice."
   :url "https://github.com/otto-de/tesla-jetty"
   :license {:name "Apache License 2.0"
